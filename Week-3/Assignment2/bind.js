a400ed24a5923307e427717674097dd7be776335
rahul0218 <kumar.rahul0218@gmail.com>
2022-05-27 22:05:43 +0530

    call function

--- Week-3/Assignment2/bind.js
+++ Week-3/Assignment2/bind.js
@@ -1,3 +1,4 @@
+console.log('==============bind function==========================')
 //bind
 
 function sum(a,b){
@@ -9,3 +10,21 @@ const sumB = sum.bind({a:10,b:20});
 const sumC = sumB.bind({a:10,b:40});
 console.log('This is output of sumB ' + sumB() +' as it is binded');
 console.log('Sum of sumC will not change as it\'s binded with sumB permanently. so output is same as sumB ' +sumC());
+
+
+console.log('==============Call function==========================')
+
+//call
+function greet(){
+    var reply=[this.animal, 'typically sleep between', this.sleepDuration].join(' ');
+    console.log(reply);
+}
+    var cats ={
+        animal: 'cats', sleepDuration: '12 and 16 hours'
+    };
+    var dogs={
+        animal: 'dogs', sleepDuration: '2 to 5 hours'
+    };
+greet.call(cats);
+greet.call(dogs);
+
