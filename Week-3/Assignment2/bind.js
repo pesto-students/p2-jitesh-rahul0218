a6680e9e3cc21bd52c0fcaa084919a8bc0bb3a65
rahul0218 <kumar.rahul0218@gmail.com>
2022-05-28 13:12:05 +0530

    Apply

--- Week-3/Assignment2/bind.js
+++ Week-3/Assignment2/bind.js
@@ -5,7 +5,7 @@ function sum(a,b){
     return this.a + this.b;
 }
 //console.log(sum());
-//! bind bindes permanently
+//! It bindes permanently
 const sumB = sum.bind({a:10,b:20});
 const sumC = sumB.bind({a:10,b:40});
 console.log('This is output of sumB ' + sumB() +' as it is binded');
@@ -15,6 +15,8 @@ console.log('Sum of sumC will not change as it\'s binded with sumB permanently. 
 console.log('==============Call function==========================')
 
 //call
+//It take argument seperetly 
+//it is usefull at complie time
 function greet(){
     var reply=[this.animal, 'typically sleep between', this.sleepDuration].join(' ');
     console.log(reply);
@@ -28,3 +30,13 @@ function greet(){
 greet.call(cats);
 greet.call(dogs);
 
+console.log('==============Apply function==========================')
+//Apply
+//It take argument as an array
+//It is use at runtime.
+var number = [5, 6, 90, 8];
+var max = Math.max.apply(null, number);
+console.log('Maximum element in array is '+max);
+
+var min = Math.min.apply(null, number);
+console.log('Minimum element in array is '+min);
