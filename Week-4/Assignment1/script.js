3bfadb4419d731c7636dd857db849f47169f99c6
rahul0218 <kumar.rahul0218@gmail.com>
2022-06-03 19:48:57 +0530

    promise

--- Week-4/Assignment1/script.js
+++ Week-4/Assignment1/script.js
@@ -1,6 +1,10 @@
-const getNumber =()=>{}
-
-const randomNumber = parseInt(Math.random() *100);
-
-
-//console.log(randomNumber);
+const getNumber = new Promise((resolve, reject)=>{
+    const randomNumber = parseInt(Math.random() *100);
+     setTimeout(()=>{
+         return (randomNumber % 5 === 0 ? reject (`rejected  number ${randomNumber}`) : resolve (`Resolve number ${randomNumber}`));
+     }, 1000);
+ 
+ })
+ getNumber.then((val)=>console.log(`Resolve - ${val}`))
+     .catch((val)=>console.error(`Reject - ${val}`))
+     .finally(()=>console.error(`Finally`));
