b2797cb7c7cfbdae86c6d51963c00b0ed3502689
rahul0218 <kumar.rahul0218@gmail.com>
2022-07-17 16:20:45 +0530

    copy to clipboard

--- Week-10/shortly/src/App.js
+++ Week-10/shortly/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
+import { useState } from 'react';
 function App() {
+  const [inputValue, setInputValue]=useState("");
   return (
     <div className="App">
       <Header/>
-      <Body/>
+      
+      <Body setInputValue={setInputValue} inputValue={inputValue}/>
       <Footer/>
     </div>
   );
--- Week-10/shortly/src/components/Body.js
+++ Week-10/shortly/src/components/Body.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+import CopyToClipboard from "react-copy-to-clipboard";
 
-function Body(){
+function Body({setInputValue}){
+  const [value, setValue] = useState("");
+  const LinkResult =({inputValue})=>
+    console.log(inputValue);
+  const [shortenLink, setShortenLink] = useState("Hello");
+  const [copied, setCopied] = useState(false);
+  
+  const handleClick = ()=>{
+    setInputValue(value);
+    setValue("");
+  }
+
+
+  useEffect(()=>{
+    const timer =setTimeout(()=>{
+      setCopied(false);
+    },150);
+
+    return () => clearTimeout(timer);
+  },[copied])
+  
     return <div>
         <section class="landing">
             <div class="landing-text">
@@ -22,17 +43,22 @@ function Body(){
       <div class="url-shorten-feature">
         <form class="url-shorten-form" id="url-shorten-form">
           <div>
-            <input type="text" class="url-input" placeholder="Shorten a link here..." autocomplete="off" />
+            <input type="text" class="url-input" placeholder="Shorten a link here..." value={value} onChange={e=>setValue(e.target.value)}/>
             <span class="alert"></span>
           </div>
-          <button type="submit" class="btn btn-lg btn-plus-icon">Shorten It!</button>
+          <button onClick={handleClick} type="submit" class="btn btn-lg btn-plus-icon">Shorten It!</button>
         </form>
         <div class="url-shorten-results">
+            <p>{shortenLink}</p>
+            <CopyToClipboard text={shortenLink} onCopy={()=>setCopied(true)}>
+            <button className={copied ? "copied" : ""}>Copy</button>
+            </CopyToClipboard>
             
         </div>
       </div>
 
     </div>
     </div>
+  
 }
 export default Body;
