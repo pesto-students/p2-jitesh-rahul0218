6f56103322a1f5545d1f70b41a8d4f374381d775
rahul0218 <kumar.rahul0218@gmail.com>
2022-07-22 22:56:20 +0530

    updated state

--- Week-11/lightswitch/src/index.js
+++ Week-11/lightswitch/src/index.js
@@ -1,33 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
- //import App from './App';
- //import reportWebVitals from './reportWebVitals';
+import createStore from 'redux';
+import { Provider, connect } from 'react-redux';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
 
-class Room extends React.Component{
+const initialState = {
+  isLightOn: true
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'FLIP':
+      return {
+        ...state,
+        isLightOn: !state.isLightOn
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+class Room extends React.Component {
   state = {
- isLightOn: true
+    isLightOn: true
   };
 
-  flipLight=()=>{
-    this.setState({isLightOn:!this.state.isLightOn});
+  flipLight = () => {
+    this.setState({ isLightOn: !this.state.isLightOn });
   };
 
-  render(){
+  render() {
     const lightedness = this.state.isLightOn ? "lit" : "dark";
     return (
-      <div className ={`room ${lightedness}`}>
+      <div className={`room ${lightedness}`}>
         the room is {lightedness}
-        <br/>
+        <br />
         <button onClick={this.flipLight}>flip</button>
       </div>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  isLightOn: state.isLightOn
+});
+const ConnectedRoom = connect(mapStateToProps)(Room);
 
 
- ReactDOM.render(<Room/>,document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRoom /></Provider>, document.getElementById("root"));
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 //root.render(
   // <React.StrictMode>
